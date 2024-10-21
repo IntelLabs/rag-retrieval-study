add overwrite option. Fixes normalization of the RAGGED metric

reader/compute_ci.py
@@ -22,13 +22,14 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--datasets", nargs='+', default=['asqa', 'nq']) 
     parser.add_argument("-f", "--subfolder", type=str, default=None) 
     parser.add_argument("-m", "--models", nargs='+', default=['Llama', 'Mistral'])
-    parser.add_argument("-r", "--retrievers", nargs='+', default=['gold', 'closedbook', 'bge-base', 'colbert'])
+    parser.add_argument("-c", "--conditions", nargs='+', default=['gold', 'closedbook', 'bge-base', 'colbert'])
+    parser.add_argument("--overwrite", action="store_true")
     args = parser.parse_args()
 
     # Set keywords to look for in the file names
     datasets = args.datasets  #['asqa', 'nq', 'qampari']
     models = args.models  #['Llama', 'Mistral']
-    conditions = args.retrievers  #['gold', 'closedbook', 'bge-base', 'colbert']
+    conditions = args.conditions
     
     # Figure out which citation metrics to compute CIs for, in addition to Exact Match Recall
     citation_measures = ['citation_rec', 'citation_prec']
@@ -43,25 +44,25 @@ if __name__ == "__main__":
                 for fname in score_list:
                     fstr = str(fname)
                     fname_agg = fstr[:-20] + '-mean-ci.score'
-                    if os.path.exists(fname_agg):
+                    if os.path.exists(fname_agg) and (not args.overwrite):
                         print(f"{fname_agg} exists, continuing...")
                         continue
                     else:
-                        stat_list = {} #= load_json(fname_agg)
+                        stat_list = {}
                     print(fname)
                     tmp_df = load_json(fstr)
                     #tmp_df = pd.read_json(fname, encoding_errors='ignore')
                     if (dataset == 'nq') or (dataset == 'nqcite'):
-                       acc = tmp_df['ragged_substring_match'] * 100
+                        acc = np.array(tmp_df['ragged_substring_match']) * 100
                     elif dataset == 'asqa':
-                       acc = tmp_df['str_em']
+                        acc = np.array(tmp_df['str_em'])
                     elif dataset == 'qampari':
-                       acc = tmp_df['qampari_rec_top5']
+                        acc = np.array(tmp_df['qampari_rec_top5'])
                     stat_list.update(bootstrap_ci(acc, key_prefix='em_rec'))
                     if 'shot0' not in fstr:  # Skip computing citation metrics if this is the vanilla NQ task
                         for cm in citation_measures:
                             stat_list.update(bootstrap_ci(tmp_df[cm], key_prefix=cm))
                     save_json(stat_list, fname_agg)
                     print(f"Created score file {fname_agg} with CIs using perquery data")
-                    del tmp_df, acc, fname_agg
+                    del fstr, tmp_df, acc, fname_agg
  
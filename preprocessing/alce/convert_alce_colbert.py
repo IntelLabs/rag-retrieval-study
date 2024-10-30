519e7d5067bad28cb1aaaae0e24a065f41c3ff23
aleto1999 <alexleto14@gmail.com>
2024-10-30 15:32:44 -0600

    combined preproc for colbert

--- preprocessing/alce/convert_alce_colbert.py
+++ preprocessing/alce/convert_alce_colbert.py
@@ -1,18 +1,24 @@
+from file_utils import load_json, save_json
+
 import os
 import pandas as pd
-from file_utils import load_json, save_json
 import argparse
+
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-"""
-convert alce queries to ColBERT format: .tsv with id and passage (no header)
-"""
 DATASET_PATH = os.environ.get("DATASET_PATH")
 
+
 def main(args):
+    """
+    set up alce data (qampari/asqa with DPR wiki docs) for ColBERT retrieval:
+        1. convert alce queries to ColBERT format: .tsv with id and passage (no header)
+        2. convert dpr wiki split (used by alce as docs) to ColBERT format: .tsv with id and passage (no header)
+    """
+
     input_file = os.path.join(
         DATASET_PATH,
         args.dataset,
@@ -26,7 +32,7 @@ def main(args):
     for entry in input_dict:
         if args.dataset == 'asqa':
             query_id = entry['sample_id']
-        else: 
+        else:
             query_id = entry['id']
         query_text = entry['question']
         output_dict['id'].append(query_id)
@@ -37,7 +43,7 @@ def main(args):
         args.dataset,
         "queries.tsv"
     )
-    
+
     logger.info(f"Converting dictionary to pandas df")
     df = pd.DataFrame.from_dict(output_dict, orient='columns')
 
@@ -45,8 +51,26 @@ def main(args):
     df.to_csv(output_file, sep="\t", header=False, index=False) 
 
 
-if __name__ == "__main__": 
+
+    # convert_dpr_wiki_to_colbert
+    input_file = os.path.join(
+        DATASET_PATH,
+        "dpr_wiki",
+        "raw.tsv"  # dpr wiki split used by alce as docs
+    )
+    logger.info(f"Reading input file {input_file}")
+    df = pd.read_csv(input_file, sep='\t')
+
+    output_file = os.path.join(
+        DATASET_PATH,
+        "dpr_wiki",
+        "docs.tsv"
+    )
+    logger.info(f"Writing output file {output_file}")
+    df.to_csv(output_file, sep="\t", header=False, index=False) 
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, help="Dataset to remove docs key from")
+    parser.add_argument("--dataset", type=str, help="ALCE dataset to compile eval files for: [asqa, qampari]")
     args = parser.parse_args()
     main(args)
